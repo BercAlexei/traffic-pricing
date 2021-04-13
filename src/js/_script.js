f1507140b878c47f3a8fcc18f4720872d99ec9f3
Alexei <1996Leha@mail.ru>
2021-04-13 11:47:37 +0500

    change JS
    add checked, input...

--- src/js/_script.js
+++ src/js/_script.js
@@ -5,9 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     //! создание ползунка
 
     let slider = document.querySelector('[data-slider]'),
-        btns = document.querySelector('.triangle'),
-        btnLeft = document.querySelector('.left'),
-        btnRight = document.querySelector('.right'),
         pagesPrice = document.querySelector('.form__text'),
         checkbox = document.querySelector('.checkbox'),
         check = document.querySelector('#checked'),
@@ -15,58 +12,59 @@ document.addEventListener('DOMContentLoaded', () => {
         btn = document.querySelectorAll('.button');
 
 
+        
+        function sliderProgress() {
+            let sliderProgressColor = `linear-gradient(90deg, rgb(164, 243, 235) ${slider.value}%, rgb(236, 240, 251) ${slider.value}%)`;
+                slider.style.background = sliderProgressColor;
+        }
+        sliderProgress();
 
         function textChanges(page, price) {
+
             let priceDiscount = price - (price/100 * 25);
+
             if (check.checked) {
                 pagesPrice.innerHTML = `${page} PAGEVIEWS<div class='form__price'><span>$${priceDiscount.toFixed(2)}</span>/ month</div>`
             } else {
                 pagesPrice.innerHTML = `${page} PAGEVIEWS<div class='form__price'><span>$${price.toFixed(2)}</span>/ month</div>`
             }
-            
         }
     
+        function priceChanges() {
+            if (slider.value == 0) {
+                textChanges('10K', 8)
+            } if (slider.value == 25) {
+                textChanges('50K', 12)
+            } if (slider.value == 50) {
+                textChanges('100K', 16)
+            } if (slider.value == 75) {
+                textChanges('500K', 24)
+            } if (slider.value == 100) {
+                textChanges('1M', 36)
+            }
+        }
+
+        priceChanges();
 
         checkbox.addEventListener('click', () => {
-            
+
             if (!check.checked) {
-                checkbox.classList.add('checkbox_active')
+                checkbox.classList.add('checkbox_active');
+                checkbox.classList.remove('checkbox_disabled');
             } else {
-                checkbox.classList.remove('checkbox_active')
+                checkbox.classList.remove('checkbox_active');
+                checkbox.classList.add('checkbox_disabled');
             }
+
         })
 
-        slider.addEventListener('mousemove', () => {
-            let sliderProgress = slider.value,
-                sliderProgressColor = `linear-gradient(90deg, rgb(164, 243, 235) ${sliderProgress}%, rgb(236, 240, 251) ${sliderProgress}%)`;
-    
-                btns.style.left = `calc(${sliderProgress}% - (0.4px * ${sliderProgress}))`
-    
-                slider.style.background = sliderProgressColor;
-                
-                    if (sliderProgress == 0) {
-                        textChanges('10K', 8)
-                    } if (sliderProgress == 25) {
-                        textChanges('50K', 12)
-                    } if (sliderProgress == 50) {
-                        textChanges('100K', 16)
-                    } if (sliderProgress == 75) {
-                        textChanges('500K', 24)
-                    } if (sliderProgress == 100) {
-                        textChanges('1M', 36)
-                    }
-                
+        check.addEventListener('change', priceChanges)
+
+        priceChanges();
+
+        slider.addEventListener('input', () => {
+            sliderProgress();
+            priceChanges();   
         })
-
-
-    btnLeft.addEventListener('click', (e) => {
-        e.preventDefault();
-        slider.value = +slider.value - 25;
-    })
-
-    btnRight.addEventListener('click', (e) => {
-        e.preventDefault();
-            slider.value = +slider.value + 25;
-    })
 
 });
